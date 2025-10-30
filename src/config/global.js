0432ccc4cac08f42754b8f803293036b3a9760d5
Leyson Castaño <leysofcp@hotmail.com>
2025-10-29 20:49:54 -0500

    se carga CFA

--- src/config/global.js
+++ src/config/global.js
@@ -143,7 +143,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/212500_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
